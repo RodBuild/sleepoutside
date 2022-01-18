b106a4ab1663c4cb884ed8c0bf9304df2ecb7082
chikaberry <79671470+chikaberry@users.noreply.github.com>
2022-01-18 10:31:53 -0700

    stuff

---
+++ expense/expensetracker.js
@@ -0,0 +1,12 @@
+export const apiKey = "somekey";
+const user = "me";
+
+class ExpenseTracker {
+  constructor() {
+    this.total = 0;
+    this.expenses = [];
+    this.month = month;
+  }
+
+  addExpense;
+}
---
+++ expense/main.js
@@ -0,0 +1,18 @@
+/* 
+
+create a table to display the records
+create inputs to allow entries of data, description, amount
+create add button, add event listener to add button when clicked:
+read enetered data out of inputs
+add to list of expenses , update total
+update the output table
+
+
+parentheis execute functions
+*/
+
+function submit() {
+  console.log("submitted");
+}
+
+document.querySelector("#addExpense").addEventListener("click", submit);
