4261b774b5616f55e46ea487842e00eb8494e83d
Rodrigo Rodriguez <rod19016@byui.edu>
2022-01-29 10:35:06 -0700

    team 3 - total in cart

--- src/js/utils.js
+++ src/js/utils.js
@@ -22,10 +22,28 @@ export function setClick(selector, callback) {
   qs(selector).addEventListener("click", callback);
 }
 
-// get parameter from URL
+// get parameter from URL of the website
 export function getParam(param) {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   const product = urlParams.get(param)
-  return product;   //return the parameter??
+  return product; //return the parameter??
+}
+
+// To render a list of items using template
+export function renderListTemplate(template, listElement, list, addDataTemplate) {
+  /*if (product.Id == '989CG' | product.Id == '880RT') {
+    console.log('no')
+  } 
+  else {*/
+  list.forEach(product => {
+    if (product.Id == '989CG' | product.Id == '880RT') {
+      console.log('no')
+    } else {
+      const clone = template.content.cloneNode(true);
+      const templateWithData = addDataTemplate(clone, product)
+      // insert the actual details of the current product
+      listElement.appendChild(templateWithData);
+    }
+  })
 }
